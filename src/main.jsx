edc8f9a6892b48f6335bae4e663e777e7afd1097
Ivan <109656369+altabib@users.noreply.github.com>
2024-03-02 09:55:33 +0200

    HW-2 6

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './Components/App/App'
+import App from './aaaaaaaa/App/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
